1c22fc7cd26fd31b919fe752078f9d48bb2e80b4
Gabriele Bologna <bolognagabriele2003@gmail.com>
2023-04-11 01:48:45 +0200

    First commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Security.Principal;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.Title = "WindowsKeyGetter | Made by https://github.com/GabryB03/";
+
+        if (!new WindowsPrincipal(WindowsIdentity.GetCurrent()).IsInRole(WindowsBuiltInRole.Administrator))
+        {
+            Console.WriteLine("[+] This program must be run with Administrator privileges.");
+            goto exit;
+        }
+
+        try
+        {
+            string key = WindowsKeyGrabber.GetWindowsProductKeyFromRegistry();
+        
+            if (key == null || key == "" || key.Length != 29 || !key.Contains("-"))
+            {
+                Console.WriteLine("[+] No Windows key has been found.");
+            }
+            else
+            {
+                Console.WriteLine("[+] Your Windows key is the following: " + key);
+            }
+        }
+        catch
+        {
+            Console.WriteLine("[+] No Windows key has been found.");
+        }
+
+        exit: Console.WriteLine("[+] Press ENTER to exit from the program.");
+        Console.ReadLine();
+    }
+}
